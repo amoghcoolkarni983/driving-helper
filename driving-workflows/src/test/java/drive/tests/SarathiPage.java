bbf99729fc747826b09606b17dcbd9787527f144
Amogh Kulkarni <amogh.kulkarni@cerner.com>
2021-01-13 11:37:13 -0600

    test

--- driving-workflows/src/test/java/drive/tests/SarathiPage.java
+++ driving-workflows/src/test/java/drive/tests/SarathiPage.java
@@ -27,23 +27,14 @@ public class SarathiPage extends BasePageTest {
 
         new BasePage(driver);
         reportBuilder = new ReportBuilder();
-        reportBuilder.takeScreenShot("Sarathi/Step 1");
-
-
         launchURI("MH");
-
-        reportBuilder.takeScreenShot("Sarathi/Step 2");
-
         driver.findElement(By.xpath("(//a[contains(text(),'Others')])[2]")).click();
-        reportBuilder.takeScreenShot("Sarathi/Step 3");
         driver.findElement(By.xpath("//a[contains(text(),'Camp Registration')]")).click();
         driver.findElement(By.xpath("(.//*[@type='text'])[1]")).sendKeys("2890002420");
         driver.findElement(By.xpath("(.//*[@type='text'])[2]")).sendKeys("17-01-1995");
         new ActionHandler(driver).waitForSomeTime(15000);
-        reportBuilder.takeScreenShot("Sarathi/Step 4");
+        reportBuilder.takeScreenShot("Sarathi/Step 1");
         driver.findElement(By.xpath("(.//*[@type='button'])[1]")).click();
-        new ActionHandler(driver).waitForSomeTime(10000);
-        reportBuilder.takeScreenShot("Sarathi/Step 5");
         System.out
                 .println("=====script execution ended=======");
 
